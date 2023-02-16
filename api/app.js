f08140cce965789ec98f6145354add4d2c6d6802
lostmart <lostmart@hotmail.com>
2023-02-16 15:03:42 +0100

    inisialization de l'api

---
+++ api/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+	res.json({
+		msg: 'running ...',
+	})
+})
+
+app.listen(4000, () => {
+	console.log('listening for requests on port 4000')
+})
